b6f3322eb89fb941858bd707338c7cb1348613fa
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-02-26 00:46:28 +0900

    fix deprecated option

--- src/chromium.js
+++ src/chromium.js
@@ -17,7 +17,7 @@ const getScreenshot = async (url, type, isDev) => {
   const page = await getPage(isDev);
   await page.setViewport({ width: 1200, height: 628 });
   console.log(`fetch SS... ${url}`);
-  await page.goto(url, { waitUntil: 'networkidle' });
+  await page.goto(url, { waitUntil: 'networkidle2' });
   const file = await page.screenshot({ type });
   return file;
 };
