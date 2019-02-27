7c0c84b8e4cd0485ed68f937c0fa5093ae48b716
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-02-27 20:53:55 +0900

    add putting screenshot image to s3

---
+++ src/s3.js
@@ -0,0 +1,43 @@
+const s3 = require('s3');
+const { readFileSync } = require('fs');
+const { resolve: resolvePath } = require('path');
+
+const { getEnv } = require('./env');
+const { writeTempFile } = require('./file');
+
+const putScreenshot = (file, { fileName, mimeType }, isDev) => new Promise(
+  async (resolve, reject) => {
+    const filePath = await writeTempFile(fileName, file);
+    const s3Options = JSON.parse(
+      readFileSync(resolvePath('./.aws-s3-creds.json'))
+    );
+    const client = s3.createClient({ s3Options });
+    const bucket = 'assets-deck.d3594.com';
+    const dir = 'images/formations/';
+    const objectKey = `${dir}${fileName}`;
+    const { s3origin } = getEnv(isDev);
+    const uri = `${s3origin}/${objectKey}`;
+
+    console.log(`Uploading file ${fileName} to bucket: ${bucket}`);
+    const uploader = client.uploadFile({
+      localFile: filePath,
+      s3Params: {
+        Bucket: bucket,
+        Key: objectKey,
+        ACL: 'public-read',
+        ContentType: mimeType,
+      },
+    });
+
+    uploader.on('error', reject);
+    uploader.on('end', () => {
+      console.log(`Completed. ${uri}`);
+      resolve(uri);
+    })
+  }
+);
+
+module.putScreenshot = putScreenshot;
+module.exports = {
+  putScreenshot,
+};
