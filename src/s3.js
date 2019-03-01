877a4f8e23c7df2f7af1b68b4f19ace36122b4b1
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-03-01 10:49:01 +0900

    add checking existance screenshot on s3, and refactor

--- src/s3.js
+++ src/s3.js
@@ -2,15 +2,46 @@ const AWS = require('aws-sdk');
 
 const { getEnv } = require('./env');
 
+const bucket = 'assets-deck.d3594.com';
+const dir = 'images/formations/';
+const getObjectKey = fileName => `${dir}${fileName}`;
+const getUri = (fileName, isDev) => {
+  const { s3origin } = getEnv(isDev);
+  const objectKey = getObjectKey(fileName);
+  return `${s3origin}/${objectKey}`;
+};
+
+const getS3Client = (isDev) => {
+  const { s3Options } = getEnv(isDev);
+  AWS.config.update(s3Options);
+  return new AWS.S3({ apiVersion: '2006-03-01' });
+};
+
+const existsScrennshot = (fileName, isDev) => new Promise(
+  (resolve, reject) => {
+    const client = getS3Client(isDev);
+    const objectKey = getObjectKey(fileName);
+    const uri = getUri(fileName, isDev);
+
+    console.log(`Checking file existance ${fileName} on bucket: ${bucket}`);
+    client.headObject({
+      Bucket: bucket,
+      Key: objectKey,
+    }, (err, data) => {
+      if (err) {
+        if (err.code === 'NotFound') { resolve(null); }
+        reject(err);
+      }
+      resolve(uri);
+    });
+  }
+);
+
 const putScreenshot = (file, { fileName, mimeType }, isDev) => new Promise(
   (resolve, reject) => {
-    const { s3Options, s3origin } = getEnv(isDev);
-    AWS.config.update(s3Options);
-    const client = new AWS.S3({ apiVersion: '2006-03-01' });
-    const bucket = 'assets-deck.d3594.com';
-    const dir = 'images/formations/';
-    const objectKey = `${dir}${fileName}`;
-    const uri = `${s3origin}/${objectKey}`;
+    const client = getS3Client(isDev);
+    const objectKey = getObjectKey(fileName);
+    const uri = getUri(fileName, isDev);
 
     console.log(`Uploading file ${fileName} to bucket: ${bucket}`);
     client.upload({
@@ -27,7 +58,9 @@ const putScreenshot = (file, { fileName, mimeType }, isDev) => new Promise(
   }
 );
 
+module.existsScrennshot = existsScrennshot;
 module.putScreenshot = putScreenshot;
 module.exports = {
+  existsScrennshot,
   putScreenshot,
 };
